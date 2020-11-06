a0ca38d00085a30b6d4739ee7021df58fa11476a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-11-05 21:36:34 -0500

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,8 @@ $count = 0
 
 $urls = Array.new
 
-$base_path = "/www.CopyPathAndPasteInQuotes.com" 
+$base_path = "/community.ebay.ca" 
+
 
 def strip_bad_chars(text)
     text.gsub!(/"/, "'");
@@ -19,18 +20,29 @@ def strip_bad_chars(text)
     return text
 end
 
-# Steps for Wed:
-# Take the data that is downloaded and move into the folder next here
-# Copy path and add it into line 12 
-# Maybe look at the weird CSV thing to get a better 
-# sheet than "title" and "body"
-
-
 def clean_body(text)
     text.gsub!(/(\r)?\n/, "
   ");
     text.gsub!(/\s+/, ' ');
+    # Attempt to get rid of false positives from HREFs
+    text.gsub!(/-Payments/, ' ');
+    text.gsub!(/-payments/, ' ');
+    text.gsub!(/Payments-/, ' ');
+    text.gsub!(/payments-/, ' ');
+    text.gsub!(/.payments/, ' ');
+    text.gsub!(/payments./, ' ');
+    text.gsub!(/-Payment/, ' ');
+    text.gsub!(/-payment/, ' ');
+    text.gsub!(/Payment-/, ' ');
+    text.gsub!(/payment-/, ' ');
+    text.gsub!(/.payment/, ' ');
+    text.gsub!(/payment./, ' ');
+    text.gsub!(/SellingwithPayments/, ' ');
+    text.gsub!(/ManagedPayments/, ' ');
+    text.gsub!(/cart.payments./, ' ');
+    
   
+
     # extra muscle, clean up crappy HTML tags and specify what attributes are allowed
     text = Sanitize.clean(text, :elements => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'b', 'strong', 'em', 'img', 'iframe'],
         :attributes => {
@@ -53,7 +65,6 @@ def clean_body(text)
 end
 
 
-
   def parse_html_files
     Find.find(Dir.getwd) do |file|
       if !File.directory? file and File.extname(file) == '.html'
@@ -74,25 +85,57 @@ end
         # grab the body content
         body = page.css('body').to_html
         body = clean_body(body)
-        
-        # skip bodies that don't have paypal somewhere
-        if current.match(/(old|draft|archive)/)
-          next
-        end
   
         # clean the file path
         path = File.new(file).path
-        path.gsub! $base_path, "/"
+        # path.gsub! $base_path, "/"
+
+        # clean the URL to drop html/ index.html 
+        # and to slice off the beginning of the path
+        miniPath = path.split("/").slice(6, 20)
+
+        if miniPath[0] == "443"
+          miniPath = path.split("/").slice(7, 20)
+        end 
+
+        # if miniPath[-1].include?("index.html")
+        #   miniPath[-1] = miniPath[-1].gsub("index.html", "")
+        # end 
+
+        # if miniPath[-1].include?(".html")
+        #   miniPath[-1] = miniPath[-1].gsub(".html", "")
+        # end 
+
+        miniPath = miniPath.join("/")
+
+        miniPath.gsub(",", "/")
+        miniPath.gsub(" ", "/") 
+
+        url = "https://community.ebay.ca/#{miniPath}"
         
+        # added for careers.ebayinc
+        def exclude?(string)
+          !include?(string)
+        end
+
         # if we have content, add this as a page to our page array
-        if (body.length > 0) && (body.include? "paypal")
+        # skip bodies that don't have paypal somewhere
+
+        # careers-ebayinc. ALL urls have an href with the below so false positives
+        #((body.include? "payments") && (body.exclude? "https://www.ebayinc.com/company/managed-payments/"))
+
+        if body.include?("Payments") || body.include?("payments") || 
+          body.include?("PayPal") || body.include?("payout") || 
+          body.include?("earnings") || body.include?("proceeds") ||
+          body.include?("paypal") 
+
           $count += 1
           puts "Processing " + title
   
           # insert into array
           data = {
-            'title' => title,
-            'body' => body,
+            'url' => url,
+            'title' => title
           }
   
           $urls.push data
@@ -105,17 +148,23 @@ end
   end
 
   def write_csv(posts)
-    CSV.open('urls.csv', 'w' ) do |writer|
-      writer << ["path", "title", "body"]
+    CSV.open('community_ebay_ca.csv', 'w' ) do |writer|
+      writer << ["url", "title"]
       $urls.each do |c|
-        writer << [c['path'], c['title'], c['body']]
+        writer << [c['url'], c['title']]
       end
     end
   end
 
 
   def report(count)
-    puts "#{$count} html posts were processed to #{Dir.getwd}/urls.csv"
+    puts "#{$count} html posts were processed to #{Dir.getwd}/community_ebay_ca.csv"
   end
 
   parse_html_files
+
+
+
+         # if current.match(/(old|draft|archive)/)
+        #   next
+        # end
